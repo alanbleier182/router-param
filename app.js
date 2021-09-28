411c8ec72054af0f192f42d391637fb70edb2676
alanbleier182 <alanbleier182@gmail.com>
2021-09-27 19:32:21 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 4001;
+
+const spiceRack = [
+  {
+    id: 1,
+    name: 'cardamom',
+    grams: 45,
+  },
+  {
+    id: 2,
+    name: 'pimento',
+    grams: 20,
+  },
+  {
+    id: 3,
+    name: 'cumin',
+    grams: 450,
+  },
+  {
+    id: 4,
+    name: 'sichuan peppercorns',
+    grams: 107,
+  }
+];
+
+let nextSpiceId = 5;
+
+app.use(bodyParser.json());
+
+app.param('spiceIndex', (req, res, next, id) => {
+  let spiceID = spiceRack(id);
+  try {
+    const found = spiceRack.find((spiceIndex) => {
+      return spiceIndex === spiceID;
+    })
+    if (found) {
+      req.spell = found;
+      next();
+    } else {
+      next(new Error('Spice not found')):
+    };
+    } catch (err) {
+      next(err)
+    }
+  }
+);
+
+
+app.get('/spices/', (req, res, next) => {
+  res.send(spiceRack);
+});
+
+app.post('/spices/', (req, res, next) => {
+  const newSpice = req.body.spice;
+  if (newSpice.name  && newSpice.grams) {
+    newSpice.id = nextSpiceId++;
+    spiceRack.push(newSpice);
+    res.send(newSpice);
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.get('/spices/:spiceId', (req, res, next) => {
+  const spiceId = Number(req.params.id);
+  const spiceIndex = spiceRack.findIndex(spice => spice.id === spiceId);
+  if (spiceIndex !== -1) {
+    res.send(spiceRack[spiceIndex]);
+  } else {
+    res.status(404).send('Spice not found.');
+  }
+});
+
+app.put('/spices/:spiceId', (req, res, next) => {
+  const spiceId = Number(req.params.id);
+  const spiceIndex = spiceRack.findIndex(spice => spice.id === spiceId);
+  if (spiceIndex !== -1) {
+    spiceRack[spiceIndex] = req.body.spice;
+    res.send(spiceRack[spiceIndex]);
+  } else {
+    res.status(404).send('Spice not found.');
+  }
+});
+
+app.delete('/spices/:spiceId', (req, res, next) => {
+  const spiceId = Number(req.params.id);
+  const spiceIndex = spiceRack.findIndex(spice => spice.id === spiceId);
+  if (spiceIndex !== -1) {
+    spiceRack.splice(spiceIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send('Spice not found.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
